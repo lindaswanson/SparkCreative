7beb34b323fabbf9b94d35305b974009e9062940
Linda Swanson <linda.a.swanson@gmail.com>
2025-05-15 14:22:27 -0400

    Initial Commit

---
+++ SparkCreative Watch App/SparkCreativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkCreativeApp.swift
+//  SparkCreative Watch App
+//
+//  Created by Linda Swanson on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkCreative_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SparkCreative/SparkCreativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkCreativeApp.swift
+//  SparkCreative
+//
+//  Created by Linda Swanson on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkCreativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
